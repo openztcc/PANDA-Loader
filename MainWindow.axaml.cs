3c332c5b636ba4f9d4d49364cbb87ef7c851f10b
Eric Galvan <lericgalvan@outlook.com>
2023-12-20 14:53:03 -0800

    sidebar stretch to fill

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,13 +1,25 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
 
 namespace PandaLdr
 {
     public partial class MainWindow : Window
     {
+        public List<string> MyList { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            MyList = new List<string>();
+            PopulateList();
+        }
+
+        private void PopulateList()
+        {
+            MyList.Add("Item 1");
+            MyList.Add("Item 2");
+            MyList.Add("Item 3");
         }
 
         private void InitializeComponent()
