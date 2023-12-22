cbe48bb189db76130b5fe67952e2c7c523e7dd85
Eric Galvan <lericgalvan@outlook.com>
2023-12-22 06:38:14 -0800

    updates to mod model

--- Models/ModModel.cs
+++ Models/ModModel.cs
@@ -5,10 +5,18 @@
     * in the local Panda database.
 */
 
+using System;
+
 public class ModModel
 {
     public int Id { get; set; } // Primary Key
     public string Name { get; set; } // Name of the mod
+    public string Author { get; set; } // Author of the mod
     public bool Status { get; set; } // Is the mod enabled or disabled
     public string Category { get; set; } // Category of the mod
+    public string FileLocation { get; set; } // Location of the mod file
+    public string Tags { get; set; } // Tags associated with the mod
+    public String Notes { get; set; } // Notes associated with the mod
+    public bool IsMissing { get; set; } // Is the mod missing
+    public String FileName { get; set; } // Name of the mod file
 }
