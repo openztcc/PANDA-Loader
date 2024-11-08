163ea0a2732335cf69936062e95a1750da05ba73
Eric Galvan <lericgalvan@outlook.com>
2024-11-08 00:35:22 -0800

    Launch button works

--- Views/HomeView.axaml.cs
+++ Views/HomeView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using System.Diagnostics;
+using Avalonia.Interactivity;
+using System;
 
 namespace PandaLdr.Views
 {
@@ -8,5 +11,31 @@ namespace PandaLdr.Views
         {
             InitializeComponent();
         }
+        private void LaunchGame(object? sender, RoutedEventArgs e)
+        {
+            string path = "C:\\Program Files (x86)\\Microsoft Games\\Zoo Tycoon\\zoo.exe";
+            if (System.IO.File.Exists(path))
+            {
+
+
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = path,
+                        UseShellExecute = true
+                    });
+                }
+                catch (System.ComponentModel.Win32Exception exc)
+                {
+                    Console.WriteLine("Error: " + exc.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("zoo.exe not found at {path}");
+            }
+        }
+
     }
 }
