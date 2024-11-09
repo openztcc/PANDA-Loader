937088a5c7aca2ac79ec6b480006d3753d558f8e
Eric Galvan <lericgalvan@outlook.com>
2024-11-08 21:33:00 -0800

    Working 'installed mods' counter

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,10 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using PandaLdr.Models;
+using System;
 
 namespace PandaLdr.ViewModels
 {
-    internal class HomeViewModel : ViewModelBase
+    public partial class HomeViewModel : ViewModelBase
     {
+        private string _modPath = "C:\\Program Files (x86)\\Microsoft Games\\Zoo Tycoon\\dlupdate";
 
+        [ObservableProperty]
+        private int _installedModCount;
+
+        public HomeViewModel()
+        {
+            InstalledModCount = ArchiveMgr.GetArchives(_modPath).Count;
+            Console.WriteLine("Installed mods: " + InstalledModCount);
+        }
     }
 }
