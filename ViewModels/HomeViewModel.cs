c92ca6c775614a9ef69a9e72ea1bf746d2c77a1f
Eric Galvan <lericgalvan@outlook.com>
2024-11-08 21:50:10 -0800

    Implemented MapMgr, 'installed maps' counter

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -8,14 +8,20 @@ namespace PandaLdr.ViewModels
     public partial class HomeViewModel : ViewModelBase
     {
         private string _modPath = "C:\\Program Files (x86)\\Microsoft Games\\Zoo Tycoon\\dlupdate";
+        private string _mapPath = "C:\\Program Files (x86)\\Microsoft Games\\Zoo Tycoon\\maps";
 
         [ObservableProperty]
         private int _installedModCount;
+        [ObservableProperty]
+        private int _installedMapCount;
 
         public HomeViewModel()
         {
             InstalledModCount = ArchiveMgr.GetArchives(_modPath).Count;
             Console.WriteLine("Installed mods: " + InstalledModCount);
+
+            InstalledMapCount = MapMgr.GetMaps(_mapPath).Count;
+            Console.WriteLine("Installed maps: " + InstalledMapCount);
         }
     }
 }
