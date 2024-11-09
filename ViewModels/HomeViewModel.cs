3c515ba249feb6478b716fd5184ed9c8818549a2
Eric Galvan <lericgalvan@outlook.com>
2024-11-09 11:35:35 -0800

    Preliminary UI for zoo.ini configuration

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -19,6 +19,27 @@ namespace PandaLdr.ViewModels
         [ObservableProperty]
         private string _oztVersion = "0.1.0";
 
+        // INI Configuration
+
+        // Display Settings
+        [ObservableProperty]
+        private bool _fullScreen;
+        [ObservableProperty]
+        private bool _resolution;
+        [ObservableProperty]
+        private bool _updateRate;
+        [ObservableProperty]
+        private bool _drawRate;
+        [ObservableProperty]
+
+        // User Settings Section
+        private bool _lastFile;
+        private bool _unlockEntity0;
+        private bool _unlockCount;
+        private bool _unlockEntity1;
+        private bool _unlockEntity2;
+        private bool _showUserEntityWarning;
+
         public HomeViewModel()
         {
             InstalledModCount = ArchiveMgr.GetArchives(_modPath).Count;
