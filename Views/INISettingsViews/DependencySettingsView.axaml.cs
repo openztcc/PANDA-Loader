2ffa1855bfbc73cad8b44e88676520127eabd91d
Eric Galvan <lericgalvan@outlook.com>
2025-01-11 21:56:48 -0800

    Optimization: Moved INI Settings Views into their own subsection

---
+++ Views/INISettingsViews/DependencySettingsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PandaLdr;
+
+public partial class DependencySettingsView : UserControl
+{
+    public DependencySettingsView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/INISettingsViews/ScenarioSettingsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PandaLdr;
+
+public partial class ScenarioSettingsView : UserControl
+{
+    public ScenarioSettingsView()
+    {
+        InitializeComponent();
+    }
+}
