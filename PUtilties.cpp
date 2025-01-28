382143af4fd60082f4ddc4af3a74aba4db0763c0
Eric Galvan <lericgalvan@outlook.com>
2025-01-27 23:50:54 -0800

    Simple implementation of launchZT

---
+++ PUtilties.cpp
@@ -0,0 +1,26 @@
+#include "PUtilties.h"
+#include <QProcess>
+#include <QMutex>
+#include <QString>
+
+namspace PUtilities
+{
+    int launchZT(const QString &path) {
+        QMutexLocker locker(&mutex);
+
+        // has proc started
+        QProcess proc;
+        if (!QFile::exists(path)) {
+            qWarning() << "Zoo Tycoon 1 binary not found: " << path;
+            return 0;
+        }
+
+        // start proc
+        if (proc.startDetached(path)) {
+            return 1;
+        } else {
+            qWarning() << "Failed to start Zoo Tycoon 1 binary: " << path;
+            return 0;
+        }
+    }
+}
---
+++ PUtilties.h
@@ -0,0 +1,12 @@
+#ifndef PUTILTIES_H
+#define PUTILTIES_H
+
+#include <QObject>
+
+// static functions
+
+namespace PUtilities {
+    int launchZT(const QString &path);
+}
+
+#endif // PUTILTIES_H
