72a61d8668dea415a90937cb456bd52f2b893fb7
Eric Galvan <lericgalvan@outlook.com>
2025-01-30 15:52:01 -0800

    Declarations for PController methods

--- PController.h
+++ PController.h
@@ -5,12 +5,38 @@
 concerns itself with the mods list and operations over other classes from the UI. */
 
 #include <QObject>
+#include <QAbstractListModel>
 
-class PController
+class PController : public QAbstractListModel
 {
     Q_OBJECT
+
+    Q_P
 public:
+    explicit PController(QObject *parent = nullptr);
+    int currentlySelectedMod() const;
+    int modCount() const;
+    void addMod(const QString &modName);
+    void removeMod(int index);
+    void selectMod(int index);
+    void deselectMod();
+    void clearSlection();
+
     PController();
+    ~PController();
+    virtual int rowCount(const QModelIndex &parent) const override;
+    virtual QVariant data(const QModelIndex &index, int role) const override;
+    virtual QHash<int, QByteArray> roleNames() const override;
+
+signals:
+    void modAdded(int index);
+    void modRemoved(int index);
+    void modSelected(int index);
+    void modDeselected();
+
+private:
+    QList<QString> m_mods;
+    int m_currentIndex;
 };
 
 #endif // PCONTROLLER_H
