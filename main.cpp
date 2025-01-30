08fcbd583efe07c6dfc4fa032cd13ed854e47e73
Eric Galvan <35750901+GoosiferIO@users.noreply.github.com>
2025-01-29 21:03:54 -0800

    [Feature] Launch button launches Zoo Tycoon (#14)
    * Simple implementation of launchZT
    
    * Attempt at invokable methods and QML registration of functions
    
    * Launch button launches Zoo Tycoon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "PState.h"
+#include <QObject>
+#include <QJSValue>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
+
+    PState *p_state = new PState(&app);
+    qmlRegisterSingletonInstance("ozt.panda.PState", 1, 0, "PState", p_state);
+
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
