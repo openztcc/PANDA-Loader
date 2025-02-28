2b7b3f0eb740ef459e72e35df904b4962208431c
Eric Galvan <lericgalvan@outlook.com>
2025-02-27 21:40:18 -0800

    Initial PConfigMgr class design

--- PConfigMgr.h
+++ PConfigMgr.h
@@ -2,12 +2,31 @@
 #define PCONFIGMGR_H
 
 #include <QObject>
+#include <toml++/toml.h>
+#include "PZtdMgr.h"
+#include <QSettings>
+#include <QDir>
 
 class PConfigMgr
 {
     Q_OBJECT
 public:
     PConfigMgr();
-};
+    ~PConfigMgr();
+    toml::table getMetaConfig(const QString &ztdFilePath);
+    bool setMetaConfig(const QString &ztdFilePath, const toml::table &config);
+    bool removeMetaConfig(const QString &ztdFilePath);
+    bool addMetaConfig(const QString &ztdFilePath, const toml::table &config);
+    toml::table getZooIniConfig(const QString &iniPath);
+    bool updateZooIniConfig(const QString &iniPath, const toml::table &config);
+    bool removeZooIniConfig(const QString &iniPath);
 
-#endif // PCONFIGMGR_H
+private:
+    PZtdMgr m_ztdMgr;
+    QSettings m_settings;
+    const QString m_zooIniName = "zoo.ini";
+    const QString m_metaConfigName = "meta.toml";
+    const QString m_configDirPath = QDir::homePath() + "/.config/PandaLdr/"; // temporary
+    const QString m_metaConfigDirPath = m_configDirPath + "meta_configs/"; // temporary
+};
+    #endif // PCONFIGMGR_H
