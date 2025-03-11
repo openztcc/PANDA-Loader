d745683c3d32422ee00d318de86befbc5fb9233e
Eric Galvan <lericgalvan@outlook.com>
2025-03-11 09:30:59 -0700

    Implementations for constructor, openDatabase, closeDatabase

--- PDatabaseMgr.cpp
+++ PDatabaseMgr.cpp
@@ -1,4 +1,25 @@
 #include "PDatabaseMgr.h"
 
-PDatabaseMgr::PDatabaseMgr() {}
+PDatabaseMgr::PDatabaseMgr() {
+    m_db = QSqlDatabase::addDatabase("QSQLITE");
+    m_db.setDatabaseName(m_dbName);
+
+    if (!openDatabase()) {
+        qDebug() << "Failed to open database";
+    }
+}
+
 PDatabaseMgr::~PDatabaseMgr() {}
+
+bool PDatabaseMgr::openDatabase() {
+    if (!m_db.open()) {
+        qDebug() << "Failed to open database: " << m_db.lastError();
+        return false;
+    }
+
+    return true;
+}
+
+void PDatabaseMgr::closeDatabase() {
+    m_db.close();
+}
