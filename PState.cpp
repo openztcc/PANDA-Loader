117049146afffeab972a999335b12f9083d1535e
Eric Galvan <lericgalvan@outlook.com>
2025-03-12 04:53:02 -0700

    PState impl: setPath, getPath, loadMods

--- PState.cpp
+++ PState.cpp
@@ -2,6 +2,7 @@
 
 PState::PState(QObject *parent) : QObject(parent) {
     m_path = "C:\\Program Files (x86)\\Microsoft Games\\Zoo Tycoon\\zoo.exe";
+    m_mods = QVector<PModItem>();
 }
 
 int PState::launchZT() {
@@ -24,4 +25,48 @@ int PState::launchZT() {
     }
 
     return 0;
+}
+
+void PState::setPath(QString path) {
+    m_path = path;
+    emit pathChanged();
+}
+
+QString PState::getPath() {
+    return m_path;
+}
+
+QStringList PState::getZtdList() {
+    // Get list of ztd files in m_path
+    QDir dir(m_path);
+    QStringList filters;
+    filters << "*.ztd";
+    QStringList ztdList = dir.entryList(filters, QDir::Files, QDir::Name);
+
+    // Print list of ztd files
+    for (const QString &ztd : ztdList) {
+        qDebug() << ztd;
+    }
+
+    return ztdList;
+}
+
+void PState::loadMods() {
+    // Scan m_path for mods
+    QStringList ztdList = getZtdList();
+
+    // Load mods into m_mods
+    for (const QString &ztd : ztdList) {
+        PModItem mod;
+        toml::table config = PConfigMgr::getMetaConfig(ztd);
+        mod.setmodTitle(PConfigMgr::getKeyValue("title", config));
+        mod.setmodAuthor(PConfigMgr::getKeyValue("authors", config));
+        mod.setmodDescription(PConfigMgr::getKeyValue("description", config));
+        mod.setVersion(PConfigMgr::getKeyValue("version", config));
+        mod.setmodPath(ztd);
+        mod.setmodEnabled(true);
+        mod.setmodTags(PConfigMgr::getKeyValue("tags", config));
+        m_mods.append(mod);
+    }
+
 }
--- PState.h
+++ PState.h
@@ -8,6 +8,12 @@
 #include <QDebug>
 #include <QFile>
 #include <QQmlEngine>
+#include "PModItem.h"
+#include <QVector>
+#include "PZtdMgr.h"
+#include "PDatabaseMgr.h"
+#include "PConfigMgr.h"
+#include "toml.hpp"
 
 // static functions
 
@@ -25,9 +31,12 @@ signals:
     void pathChanged();
     QString getPath();
     void setPath(QString);
+    static void loadMods();
 
 private:
     QString m_path;// = "C:\\Program Files (x86)\\Microsoft Games\\Zoo Tycoon\\zoo.exe";
+    QVector<PModItem> m_mods;
+    QStringList getZtdList();
 };
 
 #endif // PSTATE_H
