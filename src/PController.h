db76c5b42aee151c94852b609925212ec212c0c9
Eric Galvan <lericgalvan@outlook.com>
2025-03-21 14:10:03 -0700

    Initial implementation for updateModList

--- src/PController.h
+++ src/PController.h
@@ -53,6 +53,7 @@ public:
     void loadMods();
     void loadModsFromZTDs(const QStringList &ztdList);
     void addState(PState *state);
+    void updateModList(QString orderBy, QString searchTerm);
 
     virtual int rowCount(const QModelIndex &parent) const override;
     virtual QVariant data(const QModelIndex &index, int role) const override;
