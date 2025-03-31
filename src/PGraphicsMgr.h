afd27d40ba852df760f44e6c20440bf474cd8d02
Eric Galvan <lericgalvan@outlook.com>
2025-03-30 21:18:30 -0700

    Added ApeCore as a dependency

---
+++ src/PGraphicsMgr.h
@@ -0,0 +1,13 @@
+#ifndef PGRAPHICSMGR_H
+#define PGRAPHICSMGR_H
+
+#include <QObject>
+
+class PGraphicsMgr : public QObject
+{
+    Q_OBJECT
+public:
+    PGraphicsMgr();
+};
+
+#endif // PGRAPHICSMGR_H
