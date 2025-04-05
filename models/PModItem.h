2b53885f8884f2b6f2236c49cf2b21964f0625da
Eric Galvan <lericgalvan@outlook.com>
2025-04-05 16:21:29 -0700

    Update PModItem model with new fields

--- models/PModItem.h
+++ models/PModItem.h
@@ -16,11 +16,14 @@ class PModItem : public QObject
     Q_PROPERTY(QString modTitle READ modTitle WRITE setmodTitle NOTIFY modTitleChanged)
     Q_PROPERTY(QString modAuthor READ modAuthor WRITE setmodAuthor NOTIFY modAuthorChanged)
     Q_PROPERTY(QString modDescription READ modDescription WRITE setmodDescription NOTIFY modDescriptionChanged)
-    Q_PROPERTY(QUrl modPath READ modPath WRITE setmodPath NOTIFY modPathChanged)
     Q_PROPERTY(bool modEnabled READ modEnabled WRITE setmodEnabled NOTIFY modEnabledChanged)
     Q_PROPERTY(QString modCategory READ modCategory WRITE setmodCategory NOTIFY modCategoryChanged)
     Q_PROPERTY(QString modTags READ modTags WRITE setmodTags NOTIFY modTagsChanged)
     Q_PROPERTY(QString modId READ modId WRITE setmodId NOTIFY modIdChanged)
+    Q_PROPERTY(QUrl modLocation READ modLocation WRITE setmodLocation NOTIFY modLocationChanged)
+    Q_PROPERTY(QString modFilename READ modFilename WRITE setmodFilename NOTIFY modFilenameChanged)
+    Q_PROPERTY(QStringList modIconPaths READ modIconPaths WRITE setmodIconPaths NOTIFY modIconPathsChanged)
+    Q_PROPERTY(QString dependencyId READ dependencyId WRITE setDependencyId NOTIFY dependencyIdChanged)
 
     Q_PROPERTY(QObject* qmlItem READ qmlItem WRITE setQmlItem NOTIFY qmlItemChanged)
     Q_PROPERTY(bool isSelected READ isSelected WRITE setIsSelected NOTIFY isSelectedChanged)
@@ -42,9 +45,6 @@ public:
     QString modDescription() const;
     void setmodDescription(const QString &newModDescription);
 
-    QUrl modPath() const;
-    void setmodPath(const QUrl &newModPath);
-
     bool modEnabled() const;
     void setmodEnabled(bool newModEnabled);
 
@@ -56,6 +56,18 @@ public:
 
     QString modId() const;
     void setmodId(const QString &newModId);
+
+    QUrl modLocation() const { return m_mod_location; }
+    void setmodLocation(const QUrl &newModLocation) { m_mod_location = newModLocation; }
+
+    QString modFilename() const { return m_mod_filename; }
+    void setmodFilename(const QString &newModFilename) { m_mod_filename = newModFilename; }
+
+    QStringList modIconPaths() const { return m_mod_iconpaths; }
+    void setmodIconPaths(const QStringList &newModIconPaths) { m_mod_iconpaths = newModIconPaths; }
+
+    QString dependencyId() const { return m_dependency_id; }
+    void setDependencyId(const QString &newDependencyId) { m_dependency_id = newDependencyId; }
 
     QObject* qmlItem() const { return m_qmlItem; }
     void setQmlItem(QObject* item);
@@ -73,24 +85,30 @@ signals:
     void modTitleChanged();
     void modAuthorChanged();
     void modDescriptionChanged();
-    void modPathChanged();
     void modEnabledChanged();
     void modCategoryChanged();
     void modTagsChanged();
     void modIdChanged();
     void qmlItemChanged();
     void isSelectedChanged();
+    void modLocationChanged();
+    void modFilenameChanged();
+    void modIconPathsChanged();
+    void dependencyIdChanged();
 
 private:
     int m_mod_index;
     QString m_mod_title;
     QString m_mod_author;
     QString m_mod_description;
-    QUrl m_mod_path;
     bool m_mod_enabled;
     QString m_mod_category;
     QString m_mod_tags;
     QString m_mod_id;
+    QString m_mod_location;
+    QString m_mod_filename;
+    QStringList m_mod_iconpaths;
+    QString m_dependency_id;
     QObject* m_qmlItem = nullptr;
     bool m_isSelected = false;
 };
