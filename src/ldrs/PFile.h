74755cee349a42ca1836f573e6041046fcd7dc11
Eric Galvan <lericgalvan@outlook.com>
2025-04-24 01:33:28 -0700

    Implementations for PFile

--- src/ldrs/PFile.h
+++ src/ldrs/PFile.h
@@ -7,32 +7,45 @@
 // Qt
 #include <QCore>
 
+/*
+This is an abstraction layer above the two supported virtual filesystems:
+PZip and PFileSystem.
+It provides a common interface for file operations such as read, write, remove, etc.
+*/
+
 class PFileSystem : public QObject {
     Q_OBJECT
 public: 
     PFileSystem() = default;
     ~PFileSystem() override = default;
 
-    bool create(const QString &filePath);
+    std::unique_ptr<IVirtualFilesystem> createFilesystem(const QString &filePath) const;
 
     // mount point functions
     void setRootPath(const QString &path);
     QString rootPath() const;
+    bool open(const QString &filePath, int mode);
+    bool close();
 
+    // file operations - relative to root path
     PFileData read(const QString &filePath);
     bool write(const QString &filePath, const PFileData &data);
     bool remove(const QString &filePath);
     bool exists(const QString &filePath);
+
+    // move/rename
     bool move(const QString &filePath, const QString &newLocation);
     bool copy(const QString &filePath, const QString &newLocation);
     bool rename(const QString &filePath, const QString &newFileName);
     bool replace(const QString &filePath, const PFileData &data);
+
+    // directory operations
     bool makeDir(const QString &dirPath);
     bool dirExists(const QString &dirPath);
     bool removeDir(const QString &dirPath);
     bool listFiles(const QString &dirPath);
 private:
-    std::unique_ptr<IVirtualFilesystem> m_file;System; // pointer to the virtual filesystem
+    std::unique_ptr<IVirtualFilesystem> m_file; // pointer to the virtual filesystem
     QString m_rootPath; // root path of the zip file
 
 };
